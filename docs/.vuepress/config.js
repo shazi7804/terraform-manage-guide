2468a36c234703715124751779a2d09ef3f8e5d0
scott.liao <shazi7804@gmail.com>
2019-02-19 14:57:02 +0800

    fix use npm not yarn

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,11 @@
 module.exports = {
   title: 'Terraform 的一百零一種姿勢',
   description: 'Terraform manage guide',
+  base: 'terraform-manage-guide',
   themeConfig: {
     sidebar: [
       ['/', 'Terraform 的一百零一種姿勢'],
-      ['intro', 'Terraform 簡介'],
+      ['intro', '簡介'],
       ['/questions/', '已知問題']
     ],
     nav: [
